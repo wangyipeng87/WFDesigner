14f6c6e476a0dfb11631d32f5753e2ba07e10640
wangyipeng <wangyipeng@autohome.com.cn>
2021-04-27 22:47:24 +0800

    推送

---
+++ Content/js/wfframework.js
@@ -0,0 +1,75 @@
+﻿var lang = {
+    "sProcessing": "处理中...",
+    "sLengthMenu": "每页 _MENU_ 项",
+    "sZeroRecords": "没有匹配结果",
+    "sInfo": "当前显示第 _START_ 至 _END_ 项，共 _TOTAL_ 项。",
+    "sInfoEmpty": "当前显示第 0 至 0 项，共 0 项",
+    "sInfoFiltered": "(由 _MAX_ 项结果过滤)",
+    "sInfoPostFix": "",
+    "sSearch": "搜索:",
+    "sUrl": "",
+    "sEmptyTable": "表中数据为空",
+    "sLoadingRecords": "载入中...",
+    "sInfoThousands": ",",
+    "oPaginate": {
+        "sFirst": "首页",
+        "sPrevious": "上页",
+        "sNext": "下页",
+        "sLast": "末页",
+        "sJump": "跳转"
+    },
+    "oAria": {
+        "sSortAscending": ": 以升序排列此列",
+        "sSortDescending": ": 以降序排列此列"
+    }
+};
+
+$.htmlEncode = function (str) {
+    var div = document.createElement("div");
+    var text = document.createTextNode(str);
+    div.appendChild(text);
+    return div.innerHTML;
+}
+$.htmlDecode = function (str) {
+    var div = document.createElement("div");
+    div.innerHTML = str;
+    return div.innerHTML;
+}
+// 对Date的扩展，将 Date 转化为指定格式的String
+// 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符，
+// 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字)
+// 例子：
+// (new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423
+// (new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18
+//Date.prototype.Format = function (fmt) { //author: meizz
+//    var o = {
+//        "M+": this.getMonth() + 1, //月份
+//        "d+": this.getDate(), //日
+//        "h+": this.getHours(), //小时
+//        "m+": this.getMinutes(), //分
+//        "s+": this.getSeconds(), //秒
+//        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+//        "S": this.getMilliseconds() //毫秒
+//    };
+//    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+//    for (var k in o)
+//        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+//    return fmt;
+//}
+
+function ChangeDateFormat(cellval) {
+    return cellval.substring(0, 10);;
+}
+$.isempty = function (str) {
+    if (str == null || str == undefined || str == "" || $.trim(str) == "") {
+        return true;
+    }
+    return false;
+}
+var layerloadindex
+function startloading() {
+    layerloadindex = layer.load();
+}
+function closeloading() {
+    layer.close(layerloadindex);
+}
